cb200a2034ce962ea4d9b5acea0acb5132f1aea3
Hristo Yankov <ico.yankov@gmail.com>
2018-06-15 17:24:42 +0100

    [hyankov] Rename users_db.clj -> users_db.edn

--- src/bike_stations/auth.clj
+++ src/bike_stations/auth.clj
@@ -1,7 +1,9 @@
 (ns bike-stations.auth)
 
+(def passwd-file "users_db.edn")
+
 (defn authenticated? [name pass]
-  (let [users-db (read-string (slurp "users_db.clj"))
+  (let [users-db (read-string (slurp passwd-file))
         user (first (filter #(= (:name %) name) users-db))]
     (when user
       (= (:password user) pass))))
